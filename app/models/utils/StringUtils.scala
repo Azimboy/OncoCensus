7c6a1de108de2bffd16ffcfb85fc0b615c7eed65
Azimboy <azimboyodilov@gmail.com>
2018-05-19 16:39:39 +0500

    Cyril to latin implementation

--- app/models/utils/StringUtils.scala
+++ app/models/utils/StringUtils.scala
@@ -9,6 +9,95 @@ object StringUtils extends LazyLogging {
 
   private val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
 
+  val Cyril2Latin = Map(
+    'А' -> "A",
+    'Б' -> "B",
+    'В' -> "V",
+    'Г' -> "G",
+    'Д' -> "D",
+    'Е' -> "E",
+    'Ё' -> "Yo",
+    'Ж' -> "J",
+    'З' -> "Z",
+    'И' -> "I",
+    'Й' -> "Y",
+    'К' -> "K",
+    'Л' -> "L",
+    'М' -> "M",
+    'Н' -> "N",
+    'О' -> "O",
+    'П' -> "P",
+    'Р' -> "R",
+    'С' -> "S",
+    'Т' -> "T",
+    'У' -> "U",
+    'Ф' -> "F",
+    'Х' -> "X",
+    'Ц' -> "S",
+    'Ч' -> "Ch",
+    'Ш' -> "Sh",
+    'Ў' -> "O‘",
+    'Ь' -> "",
+    'Қ' -> "Q",
+    'Ъ' -> "'",
+    'Э' -> "E",
+    'Ю' -> "Yu",
+    'Я' -> "Ya",
+    'Ҳ' -> "H",
+    'Ғ' -> "G‘",
+    'а' -> "a",
+    'б' -> "b",
+    'в' -> "v",
+    'г' -> "g",
+    'д' -> "d",
+    'е' -> "e",
+    'ё' -> "yo",
+    'ж' -> "j",
+    'з' -> "z",
+    'и' -> "i",
+    'й' -> "y",
+    'к' -> "k",
+    'л' -> "l",
+    'м' -> "m",
+    'н' -> "n",
+    'о' -> "o",
+    'п' -> "p",
+    'р' -> "r",
+    'с' -> "s",
+    'т' -> "t",
+    'у' -> "u",
+    'ф' -> "f",
+    'х' -> "x",
+    'ц' -> "s",
+    'ч' -> "ch",
+    'ш' -> "sh",
+    'ў' -> "o‘",
+    'ь' -> "",
+    'қ' -> "q",
+    'ъ' -> "'",
+    'э' -> "e",
+    'ю' -> "yu",
+    'я' -> "ya",
+    'ҳ' -> "h",
+    'ғ' -> "g‘",
+  )
+
+  def cyril2Latin(kril: String): String = {
+    val replaceYe = (c: Char) => c match {
+      case 'Е' => "Ye"
+      case 'е' => "ye"
+      case _ => Cyril2Latin.getOrElse(c, c)
+    }
+
+    kril.zipWithIndex.map { case (c, i) =>
+      if (i == 0 || kril(i - 1) == ' ') {
+        replaceYe(c)
+      } else {
+        Cyril2Latin.getOrElse(c, c)
+      }
+    }.mkString
+  }
+
   def isValidEmail(email: String): Boolean = {
     if (email == null) {
       false
